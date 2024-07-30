3638f84549fd90e72c0d3c70e64c00a3e9c0b2cd
Prakash2608 <prakashraj822@gmail.com>
2024-07-30 13:45:14 +0530

    mlflow integrated

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -4,9 +4,9 @@ from src.logger.logging import logging
 from src.exceptions.exception import customexception
 import pandas as pd
 
-from components.data_iingestion import DataIngestion
-from components.data_transformation import DataTransformation
-from components.model_training import ModelTrainer
+from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
+from src.components.model_training import ModelTrainer
 from src.components.model_evaluation import ModelEvaluation
 
 
@@ -22,5 +22,5 @@ train_arr,test_arr=data_transformation.initialize_data_transformation(train_data
 model_trainer_obj=ModelTrainer()
 model_trainer_obj.initiate_model_training(train_arr,test_arr)
 
-# model_eval_obj = ModelEvaluation()
-# model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
+model_eval_obj = ModelEvaluation()
+model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
